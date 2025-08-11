b93334db6822a9491f67cfad477f8c9b84dad7b0
Doug <jerryacktric@gmail.com>
2025-08-11 17:19:09 -0500

    Task 6

--- autoinferior/main.py
+++ autoinferior/main.py
@@ -4,21 +4,43 @@
 def main():
 
     import os
+    import sys
     from dotenv import load_dotenv
     from google import genai
+    from google.genai import types
 
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    )
+    
+    try:
+        
+        user_prompt = sys.argv[1]
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            contents=messages
+        )
+        if len(sys.argv) < 3:
+            print(response.text)
+        elif sys.argv[2] == '--verbose':
+            print(f'User prompt: {user_prompt}')
+            print(f'{response.text}')
+            print('Prompt tokens:',response.usage_metadata.prompt_token_count)
+            print('Response tokens:',response.usage_metadata.candidates_token_count)
+        
+    except:
+        print("You need to provide a question!")
+        sys.exit(1)
 
-    print(response.text)
-    print('Prompt tokens:',response.usage_metadata.prompt_token_count)
-    print('Response tokens:',response.usage_metadata.candidates_token_count)
+
+#    print(f"User prompt:{user_prompt}\n{response.text}")
+#    print('Prompt tokens:',response.usage_metadata.prompt_token_count)
+#    print('Response tokens:',response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
